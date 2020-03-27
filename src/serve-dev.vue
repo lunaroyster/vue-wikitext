fa8e8a9bab6b77c1e34dc3de5ef8bebaefe3a51b
lunaroyster <oneinaquadrillion@live.com>
2020-03-27 14:51:36 -0400

    improved demo page

--- src/serve-dev.vue
+++ src/serve-dev.vue
@@ -9,8 +9,7 @@ export default Vue.extend({
   },
   data() {
     return {
-      wikitext: `==Welcome to vue-wikitext==
-      Here's an example showing %vue-wikitext% in action.`
+      wikitext: `=Welcome to vue-wikitext=\n\n%vue-wikitext% is a vue.js component that renders wikitext. It makes use of [https://goessner.net/articles/wiky/ Stefan Gössner's Wiky] library under the hood to convert wikitext to HTML.\n\n==== Try it ====\n\nTry this component by editing the wikitext in the box below.`,
     }
   }
 });
@@ -18,6 +17,44 @@ export default Vue.extend({
 
 <template>
   <div id="app">
-    <vue-wikitext :source="wikitext" />
+    <vue-wikitext class="wikitext" :source="wikitext" />
+    <textarea id="wikitextarea" v-model="wikitext" />
   </div>
 </template>
+
+<style>
+body {
+  margin: 0em;
+}
+
+::-webkit-scrollbar {
+  width: 8px;
+  background-color: #ddd; /* or add it to the track */
+}
+::-webkit-scrollbar-thumb {
+  background-color: #aaa;
+}
+
+
+#app {
+  display: flex;
+  flex-direction: column;
+}
+.wikitext {
+  height: 60vh;
+  padding: 1em;
+  box-sizing: border-box;
+  overflow-x: auto;
+  font-family: sans-serif;
+}
+#wikitextarea {
+  width: 100%;
+  height: 40vh;
+  border: none;
+  outline: none;
+  background: #eee;
+  padding: 2em;
+  box-sizing: border-box;
+  resize: none;
+}
+</style>
