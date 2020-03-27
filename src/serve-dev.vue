a45b8f0d5380b8e205d9f6165c36f47994edaa7b
lunaroyster <oneinaquadrillion@live.com>
2020-03-27 11:59:37 -0400

    scaffolded rollup

---
+++ src/serve-dev.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import VueWikitext from '@/vueWikitext.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueWikitext
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vueWikitext />
+  </div>
+</template>
