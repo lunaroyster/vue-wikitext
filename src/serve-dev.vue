2e81126bd831144abc24296a76eda3b72f9b068e
lunaroyster <oneinaquadrillion@live.com>
2020-03-27 12:29:25 -0400

    Added vueWikitext component

--- src/serve-dev.vue
+++ src/serve-dev.vue
@@ -5,13 +5,19 @@ import VueWikitext from '@/vueWikitext.vue';
 export default Vue.extend({
   name: 'ServeDev',
   components: {
-    VueWikitext
+    'vue-wikitext': VueWikitext
+  },
+  data() {
+    return {
+      wikitext: `==Welcome to vue-wikitext==
+      Here's an example showing %vue-wikitext% in action.`
+    }
   }
 });
 </script>
 
 <template>
   <div id="app">
-    <vueWikitext />
+    <vue-wikitext :source="wikitext" />
   </div>
 </template>
---
+++ src/vueWikitext.vue
@@ -0,0 +1,31 @@
+<script>
+import wiky from 'wiky';
+
+export default {
+  name: 'VueWikitext', // vue component name
+  props: {
+    source: {
+      type: String,
+      default: '',
+    }
+  },
+  computed: {
+    html() {
+      return wiky.toHtml(this.source)
+    }
+  },
+  methods: {
+  },
+};
+</script>
+
+<template>
+  <div class="vueWikitext" v-html="html"></div>
+</template>
+
+<style scoped>
+  .vueWikitext {
+  }
+  .vueWikitext p {
+  }
+</style>
